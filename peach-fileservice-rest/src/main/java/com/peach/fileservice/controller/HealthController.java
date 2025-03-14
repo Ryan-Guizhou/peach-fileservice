75c913e916fd91ef30d65b79cee18adde20cb9c7
Ryan <103166889+Ryan-Guizhou@users.noreply.github.com>
2025-03-15 02:32:49 +0800

    feat 调试登录日志 操作日志

--- peach-fileservice-rest/src/main/java/com/peach/fileservice/controller/HealthController.java
+++ peach-fileservice-rest/src/main/java/com/peach/fileservice/controller/HealthController.java
@@ -1,11 +1,15 @@
 package com.peach.fileservice.controller;
 
+import com.peach.common.anno.UserOperLog;
+import com.peach.common.enums.ModuleEnum;
+import com.peach.common.enums.OptTypeEnum;
 import com.peach.common.response.Response;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Indexed;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,9 +26,11 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = "健康检查", value = "健康检查")
 public class HealthController {
 
-    @GetMapping
+    @GetMapping("/test/{name}")
     @ApiOperation("健康检查")
-    public Response health() {
+    @UserOperLog(moduleCode = ModuleEnum.FILSERVICE,optType = OptTypeEnum.SELECT,
+            optContent = "'health check:  params['+#p0.toString()+']'")
+    public Response health(@PathVariable("name") String name) {
         log.info("health check");
         return Response.success().setMsg("ok");
     }
