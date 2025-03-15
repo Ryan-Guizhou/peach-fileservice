ee1c092236b55c581773c865ca64ec2c56a0b0fc
Ryan <103166889+Ryan-Guizhou@users.noreply.github.com>
2025-03-16 00:45:53 +0800

    fix 调整knife4j 多模块分组应用

--- peach-fileservice-starter/src/main/java/com/peach/fileservice/starter/FileserviceStarter.java
+++ peach-fileservice-starter/src/main/java/com/peach/fileservice/starter/FileserviceStarter.java
@@ -1,19 +1,22 @@
 package com.peach.fileservice.starter;
 
 
-
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
 import com.peach.common.anno.MyBatisDao;
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.stereotype.Indexed;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Indexed;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
+@Slf4j
 @Indexed
 @Configuration
 @ComponentScan(basePackages = {"com.peach.fileservice",}, lazyInit = true)
@@ -22,13 +25,36 @@ import org.springframework.context.annotation.Configuration;
 public class FileserviceStarter {
 
 
-//    @Lazy
-//    @Bean
-//    @ConditionalOnProperty(value = "pty.store.type", havingValue = "mongoDB")
-//    public MongoClient mongoClient(@Value("${mongodb.uri}") String uri) {
-//        MongoClient mongoClient = MongoClients.create(uri);
-//        return mongoClient;
-//    }
+    @Value("${knife4j.host:http://localhost:8888}")
+    private String host;
+
+    /**
+     * @description 注册bean
+     * @author pandasF
+     * @date 2021/8/24 11:30
+     * @param:
+     * @return springfox.documentation.spring.web.plugins.Docket
+     */
+    @Lazy
+    @Bean
+    public Docket fileserviceApi() {
+        Contact contact = new Contact("PEACH","https://github.com/Ryan-Guizhou","huanhuanshu48@gmail.com");
+        Docket docket=new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(new ApiInfoBuilder()
+                        .title("PEACH-API文档")
+                        .description("PEACH-API文档")
+                        .termsOfServiceUrl(host)
+                        .contact(contact)
+                        .version("PEACH-1.0.0")
+                        .build())
+                //分组名称
+                .groupName("文件服务API")
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.peach.fileservice"))
+                .build();
+        log.error("knife4j fileservice has been configured");
+        return docket;
+    }
 
 
 }
